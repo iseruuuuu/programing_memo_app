ea2fa1d6e2593bc3eb2da27dffdffbeb70728ed9
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-12-17 18:21:01 +0900

    add: controllerを統一した

--- lib/component/memo_widget/memo_widget_controller.dart
+++ lib/component/memo_widget/memo_widget_controller.dart
@@ -1,109 +1,109 @@
-import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
-import '../../memo/memo.dart';
-import 'package:get/get.dart';
-import '../../preference/storage_service/storeage_service.dart';
-import '../../screen/home/home_screen_controller.dart';
-
-class MemoWidgetController extends GetxController {
-  final memos = <Memo>[].obs;
-  final textController = TextEditingController();
-  final int index;
-  final controller = Get.put(HomeScreenController());
-
-
-  MemoWidgetController({
-    required this.index,
-  });
-
-  @override
-  void onInit() {
-    super.onInit();
-
-    //textController.text == 'ああ';
-
-    print(index);
-
-    // // _todosに変化がある度にストレージに保存
-    // controller.worker = ever<List<Memo>>(memos, (memos) {
-    //   final data = memos.map((e) => e.toJson()).toList();
-    //   //_storage.save(data);
-    //   controller.storage.save(data);
-    // });
-
-  }
-
-  void onTapLink(String url) async {
-    //TODO 記入後の状態変化ができていない。
-    if (url != '') {
-      await launch(url);
-    } else {
-      Get.snackbar(
-        "リンクが開けませんでした",
-        "正しいURLを入力してください!!",
-        snackPosition: SnackPosition.TOP,
-        backgroundColor: Colors.white,
-      );
-    }
-  }
-
-  void onChanged(
-      {required String word, required Memo memo, required int indexs}) {
-    //wordの値だけを更新させたい。
-    //print(word);
-
-    final index = memos.indexOf(memo);
-    print(index);
-    memo.appName == word;
-
-    // final newMemo = memo.copyWith(
-    //   id: memo.id,
-    //   appName: word,
-    //   languageName: memo.languageName,
-    //   designUrlName: memo.designUrlName,
-    //   linkUrlName: memo.linkUrlName,
-    //   memo: memo.memo,
-    //   day: memo.day,
-    // );
-    final newMemos = memo.copyWith(
-      appName: word,
-    );
-    //memos.setAll(index,[newMemos]);
-
-
-
-    // controller.worker = ever<List<Memo>>(memos, (memos) {
-    //   final data = memos.map((e) => e.toJson()).toList();
-    //   //_storage.save(data);
-    //   controller.storage.save(data);
-    // });
-
-    // void updateText(String description, Todo todo) {
-    //   final index = _todos.indexOf(todo);
-    //   final newTodo = todo.copyWith(description: description);
-    //   _todos.setAll(index, [newTodo]);
-    // }
-
-    // memos.add(newMemo);
-
-    // final memo2 = Memo(
-    //   appName: '1',
-    //   languageName: '2',
-    //   linkUrlName: '4 ',
-    //   designUrlName: '5 ',
-    //   memo: '66',
-    //   day: memo.day,
-    // );
-    // memos.add(memo2);
-    //
-    // print(indexs);
-
-    //memos[index].appName == word;
-
-    //print(newMemo.appName);
-    //print(memos[index].appName);
-    //print(index);
-
-    //memos.setAll(index, [newMemo]);
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:url_launcher/url_launcher.dart';
+// import '../../memo/memo.dart';
+// import 'package:get/get.dart';
+// import '../../preference/storage_service/storeage_service.dart';
+// import '../../screen/home/home_screen_controller.dart';
+//
+// class MemoWidgetController extends GetxController {
+//   final memos = <Memo>[].obs;
+//   final textController = TextEditingController();
+//   final int index;
+//   final controller = Get.put(HomeScreenController());
+//
+//
+//   MemoWidgetController({
+//     required this.index,
+//   });
+//
+//   @override
+//   void onInit() {
+//     super.onInit();
+//
+//     //textController.text == 'ああ';
+//
+//     print(index);
+//
+//     // // _todosに変化がある度にストレージに保存
+//     // controller.worker = ever<List<Memo>>(memos, (memos) {
+//     //   final data = memos.map((e) => e.toJson()).toList();
+//     //   //_storage.save(data);
+//     //   controller.storage.save(data);
+//     // });
+//
+//   }
+//
+//   void onTapLink(String url) async {
+//     //TODO 記入後の状態変化ができていない。
+//     if (url != '') {
+//       await launch(url);
+//     } else {
+//       Get.snackbar(
+//         "リンクが開けませんでした",
+//         "正しいURLを入力してください!!",
+//         snackPosition: SnackPosition.TOP,
+//         backgroundColor: Colors.white,
+//       );
+//     }
+//   }
+//
+//   void onChanged(
+//       {required String word, required Memo memo, required int indexs}) {
+//     //wordの値だけを更新させたい。
+//     //print(word);
+//
+//     final index = memos.indexOf(memo);
+//     print(index);
+//     memo.appName == word;
+//
+//     // final newMemo = memo.copyWith(
+//     //   id: memo.id,
+//     //   appName: word,
+//     //   languageName: memo.languageName,
+//     //   designUrlName: memo.designUrlName,
+//     //   linkUrlName: memo.linkUrlName,
+//     //   memo: memo.memo,
+//     //   day: memo.day,
+//     // );
+//     final newMemos = memo.copyWith(
+//       appName: word,
+//     );
+//     //memos.setAll(index,[newMemos]);
+//
+//
+//
+//     // controller.worker = ever<List<Memo>>(memos, (memos) {
+//     //   final data = memos.map((e) => e.toJson()).toList();
+//     //   //_storage.save(data);
+//     //   controller.storage.save(data);
+//     // });
+//
+//     // void updateText(String description, Todo todo) {
+//     //   final index = _todos.indexOf(todo);
+//     //   final newTodo = todo.copyWith(description: description);
+//     //   _todos.setAll(index, [newTodo]);
+//     // }
+//
+//     // memos.add(newMemo);
+//
+//     // final memo2 = Memo(
+//     //   appName: '1',
+//     //   languageName: '2',
+//     //   linkUrlName: '4 ',
+//     //   designUrlName: '5 ',
+//     //   memo: '66',
+//     //   day: memo.day,
+//     // );
+//     // memos.add(memo2);
+//     //
+//     // print(indexs);
+//
+//     //memos[index].appName == word;
+//
+//     //print(newMemo.appName);
+//     //print(memos[index].appName);
+//     //print(index);
+//
+//     //memos.setAll(index, [newMemo]);
+//   }
+// }
--- lib/screen/home/home_screen_controller.dart
+++ lib/screen/home/home_screen_controller.dart
@@ -1,73 +1,73 @@
-import 'package:flutter/material.dart';
-
-import '../../memo/memo.dart';
-import 'package:get/get.dart';
-import 'package:intl/intl.dart';
-import '../../preference/storage_service/storeage_service.dart';
-
-class HomeScreenController extends GetxController {
-  final selectedIndex = 0.obs;
-  final memos = <Memo>[].obs;
-  final storage = TodoStorage();
-  late final Worker worker;
-  final colors = Colors.grey.obs;
-
-  @override
-  void onInit() {
-    super.onInit();
-    final storageMemos =
-        storage.load()?.map((json) => Memo.fromJson(json)).toList();
-    // SharedPreferencesにデータがなければダミー初期データをロード
-    final initialMemos = storageMemos ?? Memo.initialTodos;
-    _memos.addAll(initialMemos);
-
-
-    // _todosに変化がある度にストレージに保存
-    worker = ever<List<Memo>>(memos, (memos) {
-      final data = memos.map((e) => e.toJson()).toList();
-      storage.save(data);
-    });
-    //サンプルを入れてみる。
-    //_memos.addAll(Memo.initialTodos);
-  }
-
-  @override
-  void onClose() {
-    worker.dispose();
-    super.onClose();
-  }
-
-  List<Memo> get _memos => memos;
-
-  void onTapAddMemo() {
-    DateTime now = DateTime.now();
-    DateFormat outputFormat = DateFormat('MM/dd');
-    String date = outputFormat.format(now);
-
-    final memo = Memo(
-      appName: '',
-      languageName: '',
-      linkUrlName: '',
-      designUrlName: '',
-      memo: '',
-      day: date,
-    );
-    memos.add(memo);
-  }
-
-
-
-  //TODO backキーを打ったら消せるようにする。
-  void remove(Memo memo) {
-    memos.remove(memo);
-  }
-
-  void deleteDone() {
-    // memos.removeWhere((e) => e.id == true);
-  }
-
-  void onTap(int index, Memo memo) {
-    selectedIndex.value = index;
-    colors.value = Colors.amber;
-  }
-}
+// import 'package:flutter/material.dart';
+//
+// import '../../memo/memo.dart';
+// import 'package:get/get.dart';
+// import 'package:intl/intl.dart';
+// import '../../preference/storage_service/storeage_service.dart';
+//
+// class HomeScreenController extends GetxController {
+//   final selectedIndex = 0.obs;
+//   final memos = <Memo>[].obs;
+//   final storage = TodoStorage();
+//   late final Worker worker;
+//   final colors = Colors.grey.obs;
+//
+//   @override
+//   void onInit() {
+//     super.onInit();
+//     final storageMemos =
+//         storage.load()?.map((json) => Memo.fromJson(json)).toList();
+//     // SharedPreferencesにデータがなければダミー初期データをロード
+//     final initialMemos = storageMemos ?? Memo.initialTodos;
+//     _memos.addAll(initialMemos);
+//
+//
+//     // _todosに変化がある度にストレージに保存
+//     worker = ever<List<Memo>>(memos, (memos) {
+//       final data = memos.map((e) => e.toJson()).toList();
+//       storage.save(data);
+//     });
+//     //サンプルを入れてみる。
+//     //_memos.addAll(Memo.initialTodos);
+//   }
+//
+//   @override
+//   void onClose() {
+//     worker.dispose();
+//     super.onClose();
+//   }
+//
+//   List<Memo> get _memos => memos;
+//
+//   void onTapAddMemo() {
+//     DateTime now = DateTime.now();
+//     DateFormat outputFormat = DateFormat('MM/dd');
+//     String date = outputFormat.format(now);
+//
+//     final memo = Memo(
+//       appName: '',
+//       languageName: '',
+//       linkUrlName: '',
+//       designUrlName: '',
+//       memo: '',
+//       day: date,
+//     );
+//     memos.add(memo);
+//   }
+//
+//
+//
+//   //TODO backキーを打ったら消せるようにする。
+//   void remove(Memo memo) {
+//     memos.remove(memo);
+//   }
+//
+//   void deleteDone() {
+//     // memos.removeWhere((e) => e.id == true);
+//   }
+//
+//   void onTap(int index, Memo memo) {
+//     selectedIndex.value = index;
+//     colors.value = Colors.amber;
+//   }
+// }
